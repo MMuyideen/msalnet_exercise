6ad20fb62af1f9c4cc220a7ab36bb6423889cb6d
Mvp-Morex <100826123+Mvp-Morex@users.noreply.github.com>
2022-09-11 18:18:07 +0100

    Added project files

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Identity.Client;
+
+namespace az204_auth
+{
+    class Program
+    {
+        private const string _clientId = "3bea2a6e-8fd3-4744-80e3-e2c864105e6c";
+        private const string _tenantId = "84420c7b-e231-4d01-bff2-9399503d1a75";
+
+        public static async Task Main(string[] args)
+        {
+            var app = PublicClientApplicationBuilder
+                .Create(_clientId)
+                .WithAuthority(AzureCloudInstance.AzurePublic, _tenantId)
+                .WithRedirectUri("http://localhost")
+                .Build(); 
+            string[] scopes = { "user.read" };
+            AuthenticationResult result = await app.AcquireTokenInteractive(scopes).ExecuteAsync();
+
+            Console.WriteLine($"Token:\t{result.AccessToken}");
+        }
+    }
+}
